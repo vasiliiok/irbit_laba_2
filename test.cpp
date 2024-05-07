ff3eea9dffee7b5701d6e2d8f17260b9541f4d53
vasiliiok <starvasyaa@gmail.com>
2024-05-07 21:27:48 +0300

    commit

---
+++ test.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <ctime>
+#include "big_integer.h"
+
+
+
+int main() {
+
+}
+
+
+// Time test
+/*int main() {
+    //---------------------------------
+    uint64_t start_time = std::clock();
+    //---------------------------------
+
+    BigInt num_1("987656789876567898767896787656786567765");
+
+    //---------------------------------
+    std::cout << "Conversion from string to 2^32 base takes: " << std::clock() - start_time << " milliseconds" << std::endl;
+    //---------------------------------
+
+    std::cout << num_1;
+    return 0;
+}*/
